418179cd48d1bdece7b74d6c87348b1312ae38e6
Schell Carl Scivally <efsubenovex@gmail.com>
2023-04-10 20:41:39 +1200

    copy over phong lighting structs to scene

--- crates/renderling-shader/src/scene.rs
+++ crates/renderling-shader/src/scene.rs
@@ -57,6 +57,174 @@ pub struct GpuCamera {
     pub view: Mat4,
 }
 
+/// Calculate attenuation
+///
+/// attenuation.x: constant
+/// attenuation.y: linear
+/// attenuation.z: quadratic
+pub fn attenuate(attenuation: Vec3, distance: f32) -> f32 {
+    let level = attenuation.x + attenuation.y * distance + attenuation.z * (distance * distance);
+    if level == 0.0 {
+        // no attenuation
+        1.0
+    } else {
+        1.0 / level
+    }
+}
+
+#[cfg_attr(
+    not(target_arch = "spirv"),
+    repr(C),
+    derive(bytemuck::Pod, bytemuck::Zeroable)
+)]
+#[derive(Copy, Clone, Default)]
+pub struct GpuPointLight {
+    pub position: Vec4,
+    pub attenuation: Vec4,
+    pub ambient_color: Vec4,
+    pub diffuse_color: Vec4,
+    pub specular_color: Vec4,
+}
+
+impl GpuPointLight {
+    /// Calculate a point light's color contribution to a fragment.
+    pub fn color_phong(
+        &self,
+        vertex_pos: Vec3,
+        view: Mat4,
+        normal: Vec3,
+        camera_to_frag_dir: Vec3,
+        diffuse_color: Vec4,
+        specular_color: Vec4,
+        shininess: f32,
+    ) -> Vec3 {
+        let light_pos: Vec3 = (view * self.position().extend(1.0)).xyz();
+        let vertex_to_light = light_pos - vertex_pos;
+        let vertex_to_light_distance = vertex_to_light.length();
+
+        let light_dir: Vec3 = vertex_to_light.normalize();
+        // diffuse shading
+        let diff: f32 = normal.dot(light_dir).max(0.0);
+        // specular shading
+        let halfway_dir: Vec3 = (light_dir + camera_to_frag_dir).normalize();
+        let spec: f32 = normal.dot(halfway_dir).max(0.0).powf(shininess);
+        // attenuation
+        let distance: f32 = vertex_to_light_distance;
+        let attenuation: f32 = attenuate(self.attenuation(), distance);
+        // combine results
+        let mut ambient: Vec3 = self.ambient_color.rgb() * diffuse_color.rgb();
+        let mut diffuse: Vec3 = self.diffuse_color.rgb() * diff * diffuse_color.rgb();
+        let mut specular: Vec3 = self.specular_color.rgb() * spec * specular_color.rgb();
+        ambient *= attenuation;
+        diffuse *= attenuation;
+        specular *= attenuation;
+
+        ambient + diffuse + specular
+    }
+}
+
+
+}
+
+#[cfg_attr(
+    not(target_arch = "spirv"),
+    repr(C),
+    derive(bytemuck::Pod, bytemuck::Zeroable)
+)]
+#[derive(Copy, Clone, Default)]
+pub struct GpuSpotLight {
+    pub position: Vec4,
+    pub direction: Vec4,
+    pub attenuation: Vec4,
+    pub ambient_color: Vec4,
+    pub diffuse_color: Vec4,
+    pub specular_color: Vec4,
+    pub cutoff: Vec4,
+}
+
+impl GpuSpotLight {
+    // Calculate a spotlight's color contribution to a fragment.
+    pub fn color_phong(
+        &self,
+        vertex_pos: Vec3,
+        view: Mat4,
+        normal: Vec3,
+        camera_to_frag_dir: Vec3,
+        diffuse_color: Vec4,
+        specular_color: Vec4,
+        shininess: f32,
+    ) -> Vec3 {
+        if self.direction() == Vec3::ZERO {
+            return Vec3::ZERO;
+        }
+        let light_pos: Vec3 = (view * self.position().extend(1.0)).xyz();
+        let light_dir: Vec3 = (light_pos - vertex_pos).normalize();
+        // diffuse shading
+        let diff: f32 = normal.dot(light_dir).max(0.0);
+        // specular shading
+        let halfway_dir: Vec3 = (light_dir + camera_to_frag_dir).normalize();
+        let spec: f32 = normal.dot(halfway_dir).max(0.0).powf(shininess);
+        // attenuation
+        let distance: f32 = (light_pos - vertex_pos).length();
+        let attenuation: f32 = attenuate(self.attenuation(), distance);
+        // spotlight intensity
+        let direction: Vec3 = (-(view * self.direction().extend(0.0)).xyz()).normalize();
+        let theta: f32 = light_dir.dot(direction);
+        let epsilon: f32 = self.inner_cutoff() - self.outer_cutoff();
+        let intensity: f32 = ((theta - self.outer_cutoff()) / epsilon).clamp(0.0, 1.0);
+        // combine results
+        let mut ambient: Vec3 = self.ambient_color.rgb() * diffuse_color.rgb();
+        let mut diffuse: Vec3 = self.diffuse_color.rgb() * diff * diffuse_color.rgb();
+        let mut specular: Vec3 = self.specular_color.rgb() * spec * specular_color.rgb();
+        ambient *= attenuation * intensity;
+        diffuse *= attenuation * intensity;
+        specular *= attenuation * intensity;
+
+        ambient + diffuse + specular
+    }
+}
+
+#[cfg_attr(
+    not(target_arch = "spirv"),
+    repr(C),
+    derive(bytemuck::Pod, bytemuck::Zeroable)
+)]
+#[derive(Copy, Clone, Default)]
+pub struct GpuDirectionalLight {
+    pub direction: Vec4,
+    pub ambient_color: Vec4,
+    pub diffuse_color: Vec4,
+    pub specular_color: Vec4,
+}
+
+impl GpuDirectionalLight {
+    // Calculate a directional light's color contribution to a fragment.
+    pub fn color_phong(
+        &self,
+        view: Mat4,
+        normal: Vec3,
+        camera_to_frag_dir: Vec3,
+        diffuse_color: Vec4,
+        specular_color: Vec4,
+        shininess: f32,
+    ) -> Vec3 {
+        if self.direction() == Vec3::ZERO {
+            return Vec3::ZERO;
+        }
+        let light_dir: Vec3 = (-(view * self.direction().extend(0.0)).xyz()).normalize();
+        // diffuse shading
+        let diff: f32 = normal.dot(light_dir).max(0.0);
+        // specular shading
+        let halfway_dir: Vec3 = (light_dir + camera_to_frag_dir).normalize();
+        let spec: f32 = normal.dot(halfway_dir).max(0.0).powf(shininess);
+        // combine results
+        let ambient: Vec3 = self.ambient_color.rgb() * diffuse_color.rgb();
+        let diffuse: Vec3 = self.diffuse_color.rgb() * diff * diffuse_color.rgb();
+        let specular: Vec3 = self.specular_color.rgb() * spec * specular_color.rgb();
+        ambient + diffuse + specular
+    }
+}
+
 /// A bundle of GPU components.
 ///
 /// The fields of `GpuEntity` are all u32s that represent the
