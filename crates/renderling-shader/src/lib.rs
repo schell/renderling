a18650e7d5e0692d093f0ca5d8a560ec8104eab1
Schell Carl Scivally <efsubenovex@gmail.com>
2024-01-27 08:49:43 +1300

    removed extraneous crates

--- crates/renderling-shader/src/lib.rs
+++
@@ -1,6 +0,0 @@
-//! Shader code for `renderling`.
-//!
-//! This gathers the various `renderling-shader` crates together and
-//! exports them as a pair of uber shaders.
-#![cfg_attr(target_arch = "spirv", no_std)]
-#![deny(clippy::disallowed_methods)]
