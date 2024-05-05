e9566a1bff0a167451cd8476c9784c3507f65a5c
Schell Carl Scivally <efsubenovex@gmail.com>
2024-05-05 14:38:57 +1200

    add back tutorials/tests

---
+++ crates/renderling/src/linkage/tutorial_passthru_fragment.rs
@@ -0,0 +1,31 @@
+
+//! Automatically generated with `cd shaders && cargo run --release`.
+//!
+//! Provides the shader linkage for
+//! [tutorial::tutorial_passthru_fragment](crate::tutorial::tutorial_passthru_fragment).
+//!
+//! **source path**:
+//! `crates/renderling/src/linkage/tutorial-tutorial_passthru_fragment.spv`
+use super::ShaderLinkage;
+use std::sync::Arc;
+#[cfg(not(target_arch = "wasm32"))]
+pub const ENTRY_POINT: &str = "tutorial::tutorial_passthru_fragment";
+#[cfg(target_arch = "wasm32")]
+pub const ENTRY_POINT: &str = "tutorialtutorial_passthru_fragment";
+pub fn linkage(device: &wgpu::Device) -> ShaderLinkage {
+    ShaderLinkage {
+        module: Arc::new(device.create_shader_module(wgpu::include_spirv!(
+            "tutorial-tutorial_passthru_fragment.spv"
+        ))),
+        entry_point: ENTRY_POINT,
+    }
+}
+pub fn get_from_cache(
+    device: &wgpu::Device,
+    cache: &mut std::collections::HashMap<&'static str, Arc<ShaderLinkage>>,
+) -> Arc<ShaderLinkage> {
+    cache
+        .entry(ENTRY_POINT)
+        .or_insert_with(|| linkage(device).into())
+        .clone()
+}
